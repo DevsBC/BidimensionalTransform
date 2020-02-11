8dbca60ab942335befc5d27297175a9503f9f7d2
DevsBC <developersbac@gmail.com>
2020-02-11 16:04:14 -0700

    Curvas

--- Curves.py
+++ Curves.py
@@ -4,17 +4,19 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import math
 
+# Posicion centrada del objeto
 pos_x = 400
 pos_y = 400
 
+
 def create_circle():
     # Parametros para crear un circulo
-    radio = 100
+    radio = 300
     double_pi = math.pi * 2.0
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(0.0, 0.4, 0.2)
     glBegin(GL_POLYGON)
-    for segment in range(0, 360):
+    for segment in range(0, 360): # ciclo hasta completar el circulo
         theta = segment * double_pi / 180
         x = radio * math.cos(theta)
         y = radio * math.sin(theta)
@@ -26,11 +28,11 @@ def create_circle():
 def create_ellipse():
     # Parametros para crear un ovalo
     radio_x = 100
-    radio_y = 250
+    radio_y = 250 # Radio irregular para lograr la forma
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0, 0.4, 0.2)
+    glColor3f(0.0, 0.8, 0.1)
     glBegin(GL_LINE_LOOP)
-    for segment in range(0, 360):
+    for segment in range(0, 360): # la misma implementacion de un circulo
         theta = segment * math.pi / 180
         x = radio_x * math.cos(theta)
         y = radio_y * math.sin(theta)
@@ -40,28 +42,27 @@ def create_ellipse():
 
 
 def create_arc():
-    cx = pos_x
-    cy = pos_y
     start_angle = 0
-    arc_angle = 3
-    num_segments = 360
-    r = 100
+    arc_angle = 3.2 # la vuelta del angulo
+    num_segments = 100
+    radio = 200 # Radio del semicirculo
     theta = arc_angle / float(num_segments - 1)
-    tangetial_factor = math.tan(theta)
+    tan_factor = math.tan(theta)
     radial_factor = math.cos(theta)
-    x = r * math.cos(start_angle)
-    y = r * math.sin(start_angle)
+    x = radio * math.cos(start_angle)
+    y = radio * math.sin(start_angle)
 
+    glColor3f(0.0, 1, 2)
     glBegin(GL_LINE_STRIP)
-    for ii in range(num_segments):
-        glVertex2f(x + cx, y + cy)
-        tx = -y
-        ty = x
+    for angle in range(num_segments):
+        glVertex2f(pos_x + x, pos_y + y)
+        tan_x = -y
+        tan_y = x
 
-        x += tx * tangetial_factor
-        y += ty * tangetial_factor
+        x += tan_x * tan_factor  # forma un medio arco
+        y += tan_y * tan_factor
 
-        x *= radial_factor
+        x *= radial_factor  # multiplica por el factor del radio
         y *= radial_factor
     glEnd()
     glFlush()
@@ -70,10 +71,12 @@ def create_arc():
 def create_wave():
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(0.0, 0.4, 0.2)
+    amplitude = 10
+    frequency = 500
     glBegin(GL_POINTS)
-    for segment in range(0, 360):
-        y = 100 * math.sin(segment * (10/360))
-        glVertex2f(pos_x + segment, pos_y + y)
+    for segment in range(0, 1000):
+        y = 100 * math.sin(segment * (amplitude/frequency))
+        glVertex2f(segment, pos_y + y)
     glEnd()
     glFlush()
 
@@ -93,6 +96,7 @@ def create_spiral():
         glEnd()
     glFlush()
 
+
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
@@ -101,11 +105,14 @@ def main():
     glutCreateWindow("Figuras circulares: Juan Carlos Aranda")
     glMatrixMode(GL_PROJECTION)
     gluOrtho2D(0.0, 800.0, 0.0, 800.0)
+
     #glutDisplayFunc(create_circle)
+
     #glutDisplayFunc(create_ellipse)
+
     #glutDisplayFunc(create_arc)
-    #glutDisplayFunc(create_wave)
-    glutDisplayFunc(create_spiral)
+
+    glutDisplayFunc(create_wave)
     glutMainLoop()
 
 
