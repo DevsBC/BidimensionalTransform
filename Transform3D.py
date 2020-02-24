618eb6ed43f9f656bcacf229e928c685d1982d65
DevsBC <developersbac@gmail.com>
2020-02-23 18:15:36 -0700

    Transformaciones 3D cubo

--- Transform3D.py
+++ Transform3D.py
@@ -1,35 +1,115 @@
+# Python imports
+from math import *
+import time
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-from objects.Cube import Cube
+
+# rotation
+X_AXIS = 0.0
+Y_AXIS = 0.0
+Z_AXIS = 0.0
+DIRECTION = 1
 
 
-# The main function
+def transform_cube(transform):
+    global X_AXIS, Y_AXIS, Z_AXIS
+    global DIRECTION
+
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(500) / float(500), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    glLoadIdentity()
+    glTranslatef(0.0, 0.0, -6.0)
+
+    if transform == "traslar":
+        glTranslatef(3, 0.0, 0.0)
+        glTranslatef(0.0, 0.0, 0.0)
+        glTranslatef(0, 0.0, -6.0)
+
+    if transform == "escalar":
+        glScalef(1.2, 1.2, 1.2)  # Eje x
+
+    if transform == "rotar":
+        glRotatef(5, 1.0, 0.0, 0.0)  # Eje x
+        glRotatef(150, 0.0, 1.0, 0.0)  # Eje y
+        glRotatef(45, 0.0, 0.0, 1.0)  # Eje z
+
+    if transform == "sesgar":
+        glRotatef(150, 0.0, 1.0, 0.0)  # Eje y
+        glScalef(1.5, 1.3, .5)  # Eje x
+
+    # Draw Cube (multiple quads)
+    glBegin(GL_QUADS)
+
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(1.0, 1.0, -1.0)
+    glVertex3f(-1.0, 1.0, -1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f(1.0, 1.0, 1.0)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(1.0, -1.0, 1.0)
+    glVertex3f(-1.0, -1.0, 1.0)
+    glVertex3f(-1.0, -1.0, -1.0)
+    glVertex3f(1.0, -1.0, -1.0)
+
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(1.0, 1.0, 1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f(-1.0, -1.0, 1.0)
+    glVertex3f(1.0, -1.0, 1.0)
+
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f(1.0, -1.0, -1.0)
+    glVertex3f(-1.0, -1.0, -1.0)
+    glVertex3f(-1.0, 1.0, -1.0)
+    glVertex3f(1.0, 1.0, -1.0)
+
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f(-1.0, 1.0, -1.0)
+    glVertex3f(-1.0, -1.0, -1.0)
+    glVertex3f(-1.0, -1.0, 1.0)
+
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex3f(1.0, 1.0, -1.0)
+    glVertex3f(1.0, 1.0, 1.0)
+    glVertex3f(1.0, -1.0, 1.0)
+    glVertex3f(1.0, -1.0, -1.0)
+
+    glEnd()
+
+    X_AXIS = X_AXIS - 0.30
+    Z_AXIS = Z_AXIS - 0.30
+
+    glutSwapBuffers()
+
+
 def main():
-
-    # Initialize OpenGL
+    # Initialize the OpenGL pipeline
     glutInit(sys.argv)
 
-    # Set display mode
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    # Set OpenGL display mode
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH)
 
-    # Set size and position of window size
-    glutInitWindowSize(400, 400)
+    # Set the Window size and position
+    glutInitWindowSize(500, 500)
     glutInitWindowPosition(100, 100)
 
-    # Create window with given title
-    glutCreateWindow("Cube")
+    # Create the window with given title
+    glutCreateWindow('Tridimensional Cubo: Juan Carlos Aranda')
 
-    # Instantiate the cube
-    cube = Cube()
-
-
-    # The callback for display function
-    glutDisplayFunc(cube.create_cube())
-
-
-    # Start the main loop
+    glutIdleFunc(transform_cube("sesgar"))
+    # Run the OpenGL main loop
     glutMainLoop()
+
 
 # Call the main function
 if __name__ == '__main__':
